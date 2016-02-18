574058e720045a4f8f7a51a1860496de62565634
hirata <hirata@team-lab.com>
2016-02-18 13:56:01 +0900

    アドレスパターンにマッチしないアドレスがきても、落ちないように修正

--- modules/core/src/main/java/com/illposed/osc/OSCPortIn.java
+++ modules/core/src/main/java/com/illposed/osc/OSCPortIn.java
@@ -118,6 +118,8 @@ public class OSCPortIn extends OSCPort implements Runnable {
 				dispatcher.dispatchPacket(oscPacket);
 			} catch (IOException ex) {
 				ex.printStackTrace(); // XXX This may not be a good idea, as this could easily lead to a never ending series of exceptions thrown (due to the non-exited while loop), and because the user of the lib may want to handle this case himself
+			} catch (IllegalArgumentException ex) {
+				ex.printStackTrace();
 			}
 		}
 	}
